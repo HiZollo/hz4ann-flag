ea289e918971f3327231ffb04ce9503fc61adfd6
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-02-09 23:53:08 +0800

    chore; add popup for lv2

--- pages/levels/2.tsx
+++ pages/levels/2.tsx
@@ -1,7 +1,8 @@
+import { useState, useReducer } from "react";
+import $ from "classnames";
 import styles from '@/styles/FlipTrip.module.css'
 import { FlipTrip } from "@/utils/FlipTrip";
-import { useState } from "react";
-import $ from "classnames";
+import { PopupWrapper } from '@/components/popup'
 
 const COLORS = 3;
 const SIZE = 4;
@@ -13,16 +14,35 @@ interface ButtonProps {
   flip(position: number): void
 }
 
+interface PopupOpenType {
+  win: boolean,
+  lose: boolean
+}
+
 export default function() {
   const [game] = useState(new FlipTrip(COLORS, SIZE));
   const [ended, setEnded] = useState(false);
   const [, refresh] = useState(false);
+  const [open, updateOpen] = useReducer(
+    (prev: PopupOpenType, update: Partial<PopupOpenType>) => ({ ...prev, ...update }), 
+    { win: false, lose: false }
+  )
 
   function flip(position: number) {
     game.flip(position);
 
     if (game.endStatus) {
       setEnded(true);
+
+      if (game.endStatus === 'Win') {
+        updateOpen({ win: true }) 
+      }
+
+      if (game.endStatus === 'Lose') {
+        updateOpen({ lose: true })
+      }
+
+
       return;
     }
 
@@ -30,11 +50,19 @@ export default function() {
   }
 
   return (
-    <div className={styles.fliptrip}>{
-      Array.from({ length: SIZE }, (_, i) => {
-        return <Button key={i} position={i} color={game.state[i]} disabled={ended} flip={flip} />
-      })
-    }</div>
+    <>
+      <div className={styles.fliptrip}>{
+        Array.from({ length: SIZE }, (_, i) => {
+          return <Button key={i} position={i} color={game.state[i]} disabled={ended} flip={flip} />
+        })
+      }</div>
+      <PopupWrapper open={open.win} handleClose={() => updateOpen({ win: false })}>
+        你贏了！
+      </PopupWrapper>
+      <PopupWrapper open={open.lose} handleClose={() => updateOpen({ lose: false })}>
+        你輸了！
+      </PopupWrapper>
+    </>
   );
 }
 
