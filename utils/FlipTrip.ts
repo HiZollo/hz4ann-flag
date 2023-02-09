455e466780ab7a478fbb6d2df096bbfbc10e72c2
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2023-02-09 22:03:59 +0800

    feature: change the rule of fliptrip

--- utils/FlipTrip.ts
+++ utils/FlipTrip.ts
@@ -4,21 +4,23 @@ class FlipTrip {
   public state: number[];
   public endStatus?: string;
 
-  private perms: string[];
+  private perms: number[];
+  private permMaxSize: number;
 
   constructor(colors: number, size: number) {
-    if (colors ** size > 1024) {
-      throw new Error(`The value of size^colors should not exceed 1024.`);
-    }
     if (colors > 10) {
       throw new Error(`The number of colors should not exceed 10.`);
+    }
+    if (colors > 100) {
+      throw new Error(`The size should not exceed 100.`);
     }
 
     this.colors = colors;
     this.size = size;
     this.state = Array.from({ length: size }, () => 0);
 
-    this.perms = [this.state.join('')];
+    this.perms = [size];
+    this.permMaxSize = (size + 1) * (size + 2) / 2;
   }
 
   flip(position: number) {
@@ -27,14 +29,14 @@ class FlipTrip {
     }
 
     this.state[position] = (this.state[position] + 1) % this.colors;
-    const permString = this.state.join('');
+    const perm = this.state.reduce((acc, cur) => acc + (10 ** cur), 0);
 
-    if (this.perms.includes(permString)) {
+    if (this.perms.includes(perm)) {
       this.endStatus = 'Lose';
     }
 
-    this.perms.push(permString);
-    if (this.perms.length === this.colors ** this.size) {
+    this.perms.push(perm);
+    if (this.perms.length === this.permMaxSize) {
       this.endStatus = 'Win';
     }
   }
