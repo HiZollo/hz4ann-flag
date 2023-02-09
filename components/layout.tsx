f7565877fe617eea96a8d44a72995382d6938601
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-02-09 22:16:12 +0800

    chore: add back to mp button in the layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Head from 'next/head' 
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { BlankDiv } from './blankDiv'
+import { Button } from './button'
 
 interface WrapperProps {
   children: React.ReactNode
 }
 
 function Layout({ children }: WrapperProps) {
+  const router = useRouter()
+  const { pathname } = router
+
   return (
     <>
       <Head>
@@ -16,6 +22,7 @@ function Layout({ children }: WrapperProps) {
       <BlankDiv height="100px" />
       <main id="content">
         {children}
+        { pathname !== '/'  && <Link href="/"><Button text="回首頁" /></Link> }
         <BlankDiv />
       </main>
       <BlankDiv height="100px" />
