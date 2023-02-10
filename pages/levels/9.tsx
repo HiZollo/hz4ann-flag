c4b3e05159fbccccc25eeb9c6f37897646c28bae
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-02-11 02:13:46 +0800

    feature: make the disableCount reset everytime the user restart

--- pages/levels/9.tsx
+++ pages/levels/9.tsx
@@ -6,21 +6,27 @@ import Flags from '@/data/flags.json'
 export default function() {
   const [count, setCount] = useState(0)
   const [open, setOpen] = useState(false)
-  const { current: disableCount } = useRef(~~(Math.random() * 25) + 1)
+  const disableCount = useRef(~~(Math.random() * 25) + 1)
+
 
   useEffect(() => {
-    if (count === disableCount) setOpen(true)
+    if (count === disableCount.current) setOpen(true)
   }, [count])
+
+  const restart = () => {
+    setCount(0)
+    disableCount.current = ~~(Math.random() * 25) + 1
+  }
 
   return (
     <>
       <h1>按按鈕</h1>
       請按下方的按鈕 100 次以獲得一個 {count >= 100 ? Flags.CLICK_BUTTON : 'Flag' }
       <div  style={{ display: 'flex', flexDirection: 'row', gap: '60px', margin: '30px' }}>
-        <Button disabled={count >= disableCount} text="按鈕" onClick={() => setCount(v => v + 1)} />
-        <Button text="重來" onClick={() => setCount(0)} />
+        <Button disabled={count >= disableCount.current} text="按鈕" onClick={() => setCount(v => v + 1)} />
+        <Button text="重來" onClick={restart} />
       </div>
-      目前次數：{Math.min(disableCount, count)}
+      目前次數：{Math.min(disableCount.current, count)}
       <PopupWrapper open={open} handleClose={() => setOpen(false)}>
         按鈕被你按壞了。
       </PopupWrapper>
