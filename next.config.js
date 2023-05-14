d9feb4540069fbbae14f79130d3fc246ad3ca48a
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-05-14 15:06:38 +0800

    chore: disable obfuscator in dev mode

--- next.config.js
+++ next.config.js
@@ -3,28 +3,30 @@ const WebpackObfuscator = require('webpack-obfuscator')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.plugins.push(
-      new WebpackObfuscator({
-        compact: true,
-        debugProtection: true,
-        rotateStringArray: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.1,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.2,
-        debugProtectionInterval: 4000,
-        identifierNamesGenerator: 'hexadecimal',
-        selfDefending: true,
-        splitStrings: true,
-        stringArray: true,
-        stringArrayCallsTransform: true,
-        stringArrayCallsTransformThreshold: 0.7,
-        stringArrayEncoding: ['rc4'],
-        stringArrayShuffle: true,
-        transformObjectKeys: true,
-      }, [])
-    )
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.plugins.push(
+        new WebpackObfuscator({
+          compact: true,
+          debugProtection: true,
+          rotateStringArray: true,
+          controlFlowFlattening: true,
+          controlFlowFlatteningThreshold: 0.1,
+          deadCodeInjection: true,
+          deadCodeInjectionThreshold: 0.2,
+          debugProtectionInterval: 4000,
+          identifierNamesGenerator: 'hexadecimal',
+          selfDefending: true,
+          splitStrings: true,
+          stringArray: true,
+          stringArrayCallsTransform: true,
+          stringArrayCallsTransformThreshold: 0.7,
+          stringArrayEncoding: ['rc4'],
+          stringArrayShuffle: true,
+          transformObjectKeys: true,
+        }, [])
+      )
+    }
     return config
   }
 }
