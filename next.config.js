1c9bf4bf0f23dbf487c158f820a062e521cd46ab
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-05-15 15:35:49 +0800

    chore: change obfuscator options

--- next.config.js
+++ next.config.js
@@ -8,22 +8,23 @@ const nextConfig = {
       config.plugins.push(
         new WebpackObfuscator({
           compact: true,
-          debugProtection: true,
-          rotateStringArray: true,
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 0.1,
           deadCodeInjection: true,
-          deadCodeInjectionThreshold: 0.2,
-          debugProtectionInterval: 4000,
+          deadCodeInjectionThreshold: 0.3,
+          debugProtection: true,
+          debugProtectionInterval: 10000,
           identifierNamesGenerator: 'hexadecimal',
           selfDefending: true,
           splitStrings: true,
+          splitStringsChunkLength: 10,
           stringArray: true,
           stringArrayCallsTransform: true,
           stringArrayCallsTransformThreshold: 0.7,
           stringArrayEncoding: ['rc4'],
           stringArrayShuffle: true,
-          transformObjectKeys: true,
+          stringArrayWrappersCount: 5,
+          stringArrayWrappersType: 'variable',
+          stringArrayWrappersParametersMaxCount: 4,
+          transformObjectKeys: true
         }, [])
       )
     }
