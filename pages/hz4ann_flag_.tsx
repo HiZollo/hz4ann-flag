57586fa2aab16284dccb6ae9dfa52e3daa229fe7
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-06-25 03:10:20 +0800

    chore: add hidden url flag

---
+++ pages/hz4ann_flag_.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return <>
+    <p>　</p>
+    <p>{"{url_hackerrr-he8RKhqgg}"}</p>
+  </>
+}
---
+++ pages/levels/hz4ann_flag_.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return <>
+    <p style={{ color: "black" }}>{"{hidden_flag-somewear00000000}"}</p>
+    <p>哈哈，你以為這裡會有 flag 嗎，想太多。</p>
+  </>
+}
