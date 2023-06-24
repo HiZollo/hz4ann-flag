ee2cb36b1ed2ec5bfc8b677e92076c42c65ce03f
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-06-25 04:46:22 +0800

    feature: move morse code to 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,23 @@
+import { useState, useRef } from 'react'
+import { Button } from '@/components/button'
+
 export default function Page() {
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const [disabled, setDisabled] = useState(false)
+
+  const play = () => {
+    if (!audioRef.current) return
+    if (disabled) return
+    audioRef.current.play()
+    setDisabled(true)
+  }
+
   return <>
     <h1>404</h1>
-    喔，糟糕，你好像不小心掉入無人的荒野了。
+    <Button onClick={play} text="播放" disabled={disabled} />
+    <audio ref={audioRef}>
+      <source src="resources/404/audio.wav" />
+    </audio>
   </>
 }
 
