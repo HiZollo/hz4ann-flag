d090e83cdaf58850d8cd0cdc02802b70b3de4984
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-06-25 22:36:36 +0800

    chore: finalized obfuscator options

--- next.config.js
+++ next.config.js
@@ -2,14 +2,13 @@ const WebpackObfuscator = require('webpack-obfuscator')
 
 const ObfuscatorOptions = {
   compact: true,
-  deadCodeInjection: true,
-  deadCodeInjectionThreshold: 0.1,
   debugProtection: true,
   debugProtectionInterval: 4000,
   identifierNamesGenerator: 'hexadecimal',
   selfDefending: true,
   stringArray: true,
   stringArrayEncoding: ['rc4'],
+  stringArrayCallsTransform: true
 }
 
 /** @type {import('next').NextConfig} */
@@ -17,16 +16,6 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { dev }) => {
     if (!dev) {
-      /**
-      config.module.rules.push({
-        test: /\.js$/,
-        enforce: 'post',
-        use: {
-          loader: WebpackObfuscator.loader,
-          options: ObfuscatorOptions
-        }
-      })
-      **/
       config.plugins.push(
         new WebpackObfuscator(ObfuscatorOptions, [])
       )
